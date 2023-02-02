7927878b0e6ddd1025270c5c3476fa6e088cdc9b
Goulandis <1612160117@qq.com>
2023-02-02 10:49:03 +0800

    InsideOpenCV库初始化配置

---
+++ InsideOpenCV/Source/InsideOpenCV/InsideOpenCVGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InsideOpenCVGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INSIDEOPENCV_API AInsideOpenCVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
